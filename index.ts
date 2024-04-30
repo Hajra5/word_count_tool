e5e8edbeea94a77c1de00c185824625b97bdedbc
Hajra Irfan <hajramidjourney05@gmail.com>
2024-04-30 09:21:01 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+
+
+import inquirer from "inquirer"
+
+
+const answers:{
+    sentence:string
+}     = await inquirer.prompt([
+    {
+        
+        name :   "sentence", 
+        type :   "input",
+        message:  "Enter your Sentence to Count the Words : "
+
+    }
+])
+const words =  answers.sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`Your sentence words count is ${words.length}`);
+
+
+        
